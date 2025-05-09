4fb9e7b2c096f194b58eef798349525a6d2db2ef
sandsiva_cisco <sandsiva@cisco.com>
2025-05-09 22:31:15 +0530

    added upcoming

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -3,45 +3,45 @@ import { Blog } from "@/types/blog";
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "Best UI components for modern websites",
+    title: "Virasat",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-01.jpg",
-    author: {
-      name: "Samuyl Joshi",
-      image: "/images/blog/author-01.png",
-      designation: "Graphic Designer",
+      "Virasat is Yatra's flagship annual student showcase — a grand-scale showcase introduced in 2024 that drew a houseful audience in its debut edition. Conceived and directed by Aditya Sharma, Virasat celebrates the training, discipline, and artistic guidance offered at Yatra under his tutelage.",
+    image: "/images/blog/upcoming1.jpg",
+    location: {
+      name: "Indiranagar",
+      image: "/images/blog/upcoming1.jpg",
+      designation: "",
     },
-    tags: ["creative"],
+    tags: ["New"],
     publishDate: "2025",
-  },
-  {
-    id: 2,
-    title: "9 simple ways to improve your design skills",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-02.jpg",
-    author: {
-      name: "Musharof Chy",
-      image: "/images/blog/author-02.png",
-      designation: "Content Writer",
-    },
-    tags: ["computer"],
-    publishDate: "2025",
-  },
-  {
-    id: 3,
-    title: "Tips to quickly improve your coding speed.",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-03.jpg",
-    author: {
-      name: "Lethium Deo",
-      image: "/images/blog/author-03.png",
-      designation: "Graphic Designer",
-    },
-    tags: ["design"],
-    publishDate: "2025",
-  },
+  }
+  // {
+  //   id: 2,
+  //   title: "9 simple ways to improve your design skills",
+  //   paragraph:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+  //   image: "/images/blog/blog-02.jpg",
+  //   author: {
+  //     name: "Musharof Chy",
+  //     image: "/images/blog/author-02.png",
+  //     designation: "Content Writer",
+  //   },
+  //   tags: ["computer"],
+  //   publishDate: "2025",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Tips to quickly improve your coding speed.",
+  //   paragraph:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+  //   image: "/images/blog/blog-03.jpg",
+  //   author: {
+  //     name: "Lethium Deo",
+  //     image: "/images/blog/author-03.png",
+  //     designation: "Graphic Designer",
+  //   },
+  //   tags: ["design"],
+  //   publishDate: "2025",
+  // },
 ];
 export default blogData;
